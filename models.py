89ff0bc97a2e32388c5d04f38d09324b781268d4
Mahdi Razi Gandomani <mahdi.razi099@gmail.com>
2025-11-29 18:30:06 +0330

    Update models.py

--- models.py
+++ models.py
@@ -1,48 +1,110 @@
 import torch.nn as nn
-import torch.nn.functional as F
-from bspline_activation import LearnableBSplineActivation
+from activations import get_activation
 
 
-class SimpleNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, learnable=False):
-        super(SimpleNN, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, output_size)
-        self.activation = LearnableBSplineActivation(start_point=-2, end_point=2) if learnable else F.relu
 
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.activation(x)
-        x = self.fc2(x)
-        return x
+class MLP(nn.Module):
+    def __init__(self, in_size, hidden_sizes, out_size, act, act_cfg=None, shared_act=False):
+        super().__init__()
+        if act_cfg is None:
+            act_cfg = {}
 
-class BaseRegression(nn.Module):
-    def __init__(self):
-        super(BaseRegression, self).__init__()
-        self.fc1 = nn.Linear(1, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 32)
-        self.fc4 = nn.Linear(32, 1)
+        layers = []
+        acts = []
+        prev = in_size
+        for i, h in enumerate(hidden_sizes):
+            layers.append(nn.Linear(prev, h))
+            if shared_act and i > 0:
+                acts.append(acts[0])
+            else:
+                acts.append(get_activation(act, **act_cfg))
+            
+            prev = h
 
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = self.fc4(x)
-        return x
+        layers.append(nn.Linear(prev, out_size))
 
-class LearnableRegression(nn.Module):
-    def __init__(self):
-        super(LearnableRegression, self).__init__()
-        self.fc1 = nn.Linear(1, 64)
-        self.fc2 = nn.Linear(64, 32)
-        self.fc3 = nn.Linear(32, 1)
-
-        self.activation1 = LearnableBSplineActivation(start_point=-1, end_point=1)
-        self.activation2 = LearnableBSplineActivation(start_point=-1, end_point=1)
+        self.layers = nn.ModuleList(layers)
+        self.acts = nn.ModuleList(acts)
 
     def forward(self, x):
-        x = self.activation1(self.fc1(x))
-        x = self.activation2(self.fc2(x))
-        x = self.fc3(x)
+        x = x.view(x.size(0), -1)
+        act_idx = 0
+        for layer in self.layers[ : -1]:
+            if isinstance(layer, nn.Linear):
+                x = layer(x)
+                x = self.acts[act_idx](x)
+                act_idx += 1
+            else:
+                x = layer(x)
+        x = self.layers[-1](x)
         return x
+
+
+class CNN(nn.Module):
+    def __init__(self, in_ch=3, num_classes=10, act='relu', act_cfg=None, chans=[32,64,128], fc=[256,128]):
+        super().__init__()
+        if act_cfg is None:
+            act_cfg = {}
+
+        conv_layers = []
+        conv_acts = []
+        prev_ch = in_ch
+        for out_ch in chans:
+            conv_layers.append(nn.Conv2d(prev_ch, out_ch, 3, padding = 1))
+            conv_layers.append(nn.BatchNorm2d(out_ch))
+            conv_acts.append(get_activation(act, **act_cfg))
+            conv_layers.append(nn.MaxPool2d(2, 2))
+            prev_ch = out_ch
+
+        self.conv_layers = nn.ModuleList(conv_layers)
+        self.conv_acts = nn.ModuleList(conv_acts)
+
+        # calc fc input size
+        s = 32 // (2 ** len(chans))
+        fc_in = chans[-1] * s * s
+
+        fc_layers = []
+        fc_acts = []
+        prev = fc_in
+        for fc_size in fc:
+            fc_layers.append(nn.Linear(prev, fc_size))
+            fc_acts.append(get_activation(act, **act_cfg))
+            prev = fc_size
+        fc_layers.append(nn.Linear(prev, num_classes))
+
+        self.fc_layers = nn.ModuleList(fc_layers)
+        self.fc_acts = nn.ModuleList(fc_acts)
+
+    def forward(self, x):
+        act_idx = 0
+        for layer in self.conv_layers:
+            if isinstance(layer, nn.Conv2d):
+                x = layer(x)
+                x = self.conv_acts[act_idx](x)
+                act_idx += 1
+            else:
+                x = layer(x)
+
+        x = x.view(x.size(0), -1)
+
+        act_idx = 0
+        for layer in self.fc_layers[:-1]:
+            x = layer(x)
+            x = self.fc_acts[act_idx](x)
+            act_idx += 1
+        x = self.fc_layers[-1](x)
+        return x
+
+
+
+
+
+def create_model(model_type, in_size, out_size, act, **kwargs):
+
+    if model_type == 'mlp':
+        return MLP(in_size,kwargs.get('hidden_sizes', [256, 256]),out_size, act, kwargs.get('act_cfg', {}), kwargs.get('shared_act', False))
+        
+    elif model_type == 'cnn':
+        return CNN(in_size, out_size, act, kwargs.get('act_cfg', {}), kwargs.get('chans', [32, 64, 128]), kwargs.get('fc', [256]))
+    else:
+        raise ValueError('Unknown model')
